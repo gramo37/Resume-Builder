5e1b06a3d32e8637ef1dea2f1341cea8e9b362c0
Prasanna <gramopadhye37@gmail.com>
2022-10-28 17:34:00 +0530

    make it better

--- controllers/resumeController.js
+++ controllers/resumeController.js
@@ -29,8 +29,8 @@ exports.fetchPDF = catchAsyncError(async (req, res, next) => {
     res.sendFile(`${__dirname}/result.pdf`)
 });
 
-exports.fetchBase64PDF = catchAsyncErrors(async (req, res, next) => {
-    // Address is a new field that can be added
+// Main API - Sends PDF in base 64 format
+exports.fetchBase64PDF = catchAsyncError(async (req, res, next) => {
     let subTitle = `${req.body?.email ? req.body.email + " | " : ""} 
     ${req.body?.mobile ? req.body.mobile + " | " : ""}
     ${req.body?.github ? `<a href=${req.body?.github} target="_blank">Github</a>` + " | " : ""}
@@ -38,17 +38,17 @@ exports.fetchBase64PDF = catchAsyncErrors(async (req, res, next) => {
     ${req.body?.linkedin ? `<a href=${req.body?.linkedin} target="_blank">Linked In</a>` + " | " : ""}
     ${req.body?.personalWebsite ? `<a href=${req.body?.personalWebsite} target="_blank">Personal Website</a>` + " | " : ""}
     ${req.body?.address ? req.body.address + " | " : ""}`
-    
-    if(subTitle.charAt(subTitle.length-1) === '|') subTitle.splice(subTitle.length-1)
+
+    if (subTitle.charAt(subTitle.length - 1) === '|') subTitle.splice(subTitle.length - 1)
 
     let data = {
-      "name": req.body?.name ? req.body.name : "",
-      "subTitle": subTitle,
-      "educationalDetail": req.body?.educationalDetail ? req.body.educationalDetail : "",
-      "workExperience": req.body?.workExperience ? req.body.workExperience : [],
-      "projects": req.body?.projects ? req.body.projects : [],
-      "skills": req.body?.skills ? req.body.skills : {},
-      "extracurriculars": req.body?.extracurriculars ? req.body.extracurriculars : [],
+        "name": req.body?.name ? req.body.name : "",
+        "subTitle": subTitle,
+        "educationalDetail": req.body?.educationalDetail ? req.body.educationalDetail : [],
+        "workExperience": req.body?.workExperience ? req.body.workExperience : [],
+        "projects": req.body?.projects ? req.body.projects : [],
+        "skills": req.body?.skills ? req.body.skills : {},
+        "extracurriculars": req.body?.extracurriculars ? req.body.extracurriculars : [],
     }
 
     await pdf.create(pdfTemplate(data), {
@@ -59,24 +59,66 @@ exports.fetchBase64PDF = catchAsyncErrors(async (req, res, next) => {
             width,
             height
         }
-    }).toFile(`${__dirname}/result.pdf`, (err) => {
+    }).toBuffer(function (err, buffer) {
+        let data = Buffer.from(buffer).toString('base64');
         if (err) res.send(Promise.reject());
-        // Convert result.pdf to base 64
-        pdf2base64(`${__dirname}/result.pdf`)
-            .then(
-                (response) => {
-                    res.status(200).json({
-                        data: response
-                    })
-                }
-            )
-            .catch(
-                (error) => {
-                    res.status(404).json({
-                        data: "Something went wrong",
-                        error
-                    })
-                }
-            )
+        // res.send(Promise.resolve());
+
+        res.status(200).json({
+            success: "true",
+            data
+        })
     })
-})
+});
+
+// exports.temp = catchAsyncErrors(async (req, res, next) => {
+//     // Address is a new field that can be added
+//     let subTitle = `${req.body?.email ? req.body.email + " | " : ""} 
+//     ${req.body?.mobile ? req.body.mobile + " | " : ""}
+//     ${req.body?.github ? `<a href=${req.body?.github} target="_blank">Github</a>` + " | " : ""}
+//     ${req.body?.twitter ? `<a href=${req.body?.twitter} target="_blank">Twitter</a>` + " | " : ""}
+//     ${req.body?.linkedin ? `<a href=${req.body?.linkedin} target="_blank">Linked In</a>` + " | " : ""}
+//     ${req.body?.personalWebsite ? `<a href=${req.body?.personalWebsite} target="_blank">Personal Website</a>` + " | " : ""}
+//     ${req.body?.address ? req.body.address + " | " : ""}`
+
+//     if (subTitle.charAt(subTitle.length - 1) === '|') subTitle.splice(subTitle.length - 1)
+
+//     let data = {
+//         "name": req.body?.name ? req.body.name : "",
+//         "subTitle": subTitle,
+//         "educationalDetail": req.body?.educationalDetail ? req.body.educationalDetail : "",
+//         "workExperience": req.body?.workExperience ? req.body.workExperience : [],
+//         "projects": req.body?.projects ? req.body.projects : [],
+//         "skills": req.body?.skills ? req.body.skills : {},
+//         "extracurriculars": req.body?.extracurriculars ? req.body.extracurriculars : [],
+//     }
+
+//     await pdf.create(pdfTemplate(data), {
+//         width: `${width}px`,
+//         height: `${height}px`,
+//         border: '0px',
+//         viewportSize: {
+//             width,
+//             height
+//         }
+//     }).toFile(`${__dirname}/result.pdf`, (err) => {
+//         if (err) res.send(Promise.reject());
+//         // Convert result.pdf to base 64
+//         pdf2base64(`${__dirname}/result.pdf`)
+//             .then(
+//                 (response) => {
+//                     res.status(200).json({
+//                         data: response
+//                     })
+//                 }
+//             )
+//             .catch(
+//                 (error) => {
+//                     res.status(404).json({
+//                         data: "Something went wrong",
+//                         error
+//                     })
+//                 }
+//             )
+//     })
+// })
--- routes/resumeRoute.js
+++ routes/resumeRoute.js
@@ -1,10 +1,11 @@
 const express = require("express");
-const { createPDF, fetchPDF, fetchBase64PDF } = require("../controllers/resumeController");
+const { createPDF, fetchPDF, fetchBase64PDF, temp } = require("../controllers/resumeController");
 
 const app = express.Router();
 
 app.route('/create-pdf').post(createPDF);
 app.route('/fetch-pdf').get(fetchPDF);
 app.route('/fetch-base64-pdf').post(fetchBase64PDF);
+app.route('/temp').post(temp);
 
 module.exports = app;
